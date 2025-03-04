0aa07bf58c1ef2400989ad5edc7800bef4407cf8
Jonas Bellmann <jonas.bellmann@ivao.aero>
2025-03-04 16:39:03 +0100

    ci: add workflows
    BREAKING CHANGE: support ESLint v9

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,41 @@
-import ivaoConfig from './dist/index.js';
+import eslint from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
 
 export default [
-  ...ivaoConfig.configs.base,
-  ...ivaoConfig.configs.prettier,
+  {
+    ignores: [
+      'node_modules/**/*',
+      'dist/**/*',
+      'build/**/*',
+      'storybook-static/**/*',
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  prettier,
   {
     languageOptions: {
       parserOptions: {
         projectService: {
-          allowDefaultProject: ['eslint.config.js', 'tsup.config.ts'],
+          allowDefaultProject: [
+            'eslint.config.js',
+            'commitlint.config.ts',
+            'release.config.js',
+            'tsup.config.ts',
+          ],
         },
         tsconfigRootDir: import.meta.dirname,
       },
