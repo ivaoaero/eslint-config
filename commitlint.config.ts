670da0b88695923400e0b12ed4a8f1e56deea07c
Jonas Bellmann <jonas.bellmann@ivao.aero>
2025-12-22 13:56:55 +0100

    chore(commitlint): warn for footer length

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,4 +2,7 @@ import { UserConfig } from '@commitlint/types';
 
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'footer-max-line-length': [1, 'always', 100],
+  },
 } satisfies UserConfig;
